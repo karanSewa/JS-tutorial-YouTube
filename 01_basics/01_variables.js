447fec01648d109f255b48322442d10c83720bcb
karanSewa <100862310+karanSewa@users.noreply.github.com>
2023-07-24 09:56:07 +0000

    data types module

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 887722;
+let accountEmail = "karan221@Gmail.com";
+var accountPassword = "123456";
+accountCity = "siliguri";
+console.table([accountId, accountEmail, accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+// number = 123
+// string = "name"
+// boolean = true/false
+// null = standalone value
+// undefined => 
+// symbol => unique
+// BigInt
